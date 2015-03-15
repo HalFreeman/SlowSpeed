89ae193fe9329ba7b981fdeb588b2a668974589c
ScottNZ <stealtharcher.scott@gmail.com>
2015-03-15 14:41:37 +1300

    Restore original speeds when unloading levels

--- SlowSpeed/Mod.cs
+++ SlowSpeed/Mod.cs
@@ -15,6 +15,7 @@
  */
 
 using System;
+using System.Collections.Generic;
 using ICities;
 
 namespace SlowSpeed
@@ -34,6 +35,9 @@ namespace SlowSpeed
 
 	public class LoadingExtension : LoadingExtensionBase
 	{
+		readonly Dictionary<string, SlowVehicleInfo> vehicles = new Dictionary<string, SlowVehicleInfo>();
+		readonly Dictionary<string, SlowCitizenInfo> citizens = new Dictionary<string, SlowCitizenInfo>();
+
 		public override void OnLevelLoaded(LoadMode mode)
 		{
 			ForEachPrefab((VehicleInfo i) =>
@@ -41,25 +45,110 @@ namespace SlowSpeed
 				switch (i.m_vehicleType)
 				{
 					case VehicleInfo.VehicleType.Car:
-						i.m_maxSpeed *= 0.6f;
-						i.m_leanMultiplier *= 0.5f;
-						i.m_acceleration *= 0.25f;
-						i.m_braking *= 0.25f;
-						i.m_turning *= 0.25f;
+						{
+							var v = new SlowVehicleInfo(i);
+							vehicles.Add(i.ToString(), v);
+							v.Apply(0.6f, 0.5f, 0.25f, 0.25f, 0.25f);
+						}
 						break;
 					case VehicleInfo.VehicleType.Metro:
 					case VehicleInfo.VehicleType.Train:
-						i.m_maxSpeed *= 0.5f;
+						{
+							var v = new SlowVehicleInfo(i);
+							vehicles.Add(i.ToString(), v);
+							v.Apply(0.5f);
+						}
 						break;
 				}
 			});
-			ForEachPrefab((CitizenInfo i) => i.m_walkSpeed *= 0.25f);
+			ForEachPrefab((CitizenInfo i) =>
+			{
+				var c = new SlowCitizenInfo(i);
+				citizens.Add(i.ToString(), c);
+				c.Apply(0.25f);
+			});
+		}
+
+		public override void OnLevelUnloading()
+		{
+			foreach (var v in vehicles.Values)
+				v.Restore();
+			vehicles.Clear();
+			foreach (var c in citizens.Values)
+				c.Restore();
+			citizens.Clear();
 		}
 
 		static void ForEachPrefab<T>(Action<T> f) where T : PrefabInfo
 		{
 			for (var i = 0u; i < PrefabCollection<T>.PrefabCount(); i++)
 				f(PrefabCollection<T>.GetPrefab(i));
+		}
+	}
+
+	class SlowVehicleInfo
+	{
+		readonly VehicleInfo i;
+		public readonly float BaseMaxSpeed;
+		public readonly float BaseLeanMultiplier;
+		public readonly float BaseAcceleration;
+		public readonly float BaseBraking;
+		public readonly float BaseTurning;
+
+		public SlowVehicleInfo(VehicleInfo i)
+		{
+			this.i = i;
+			BaseMaxSpeed = i.m_maxSpeed;
+			BaseLeanMultiplier = i.m_leanMultiplier;
+			BaseAcceleration = i.m_acceleration;
+			BaseBraking = i.m_braking;
+			BaseTurning = i.m_turning;
+		}
+
+		public void Apply(float maxSpeedMultiplier, float leanMultiplierMultiplier, float accelerationMultiplier,
+			float brakingMultiplier, float turningMultiplier)
+		{
+			i.m_maxSpeed = BaseMaxSpeed * maxSpeedMultiplier;
+			i.m_leanMultiplier = BaseLeanMultiplier * leanMultiplierMultiplier;
+			i.m_acceleration = BaseAcceleration * accelerationMultiplier;
+			i.m_braking = BaseBraking * brakingMultiplier;
+			i.m_turning = BaseTurning * turningMultiplier;
+		}
+
+		public void Apply(float maxSpeedMultiplier)
+		{
+			i.m_maxSpeed = BaseMaxSpeed * maxSpeedMultiplier;
+		}
+
+		public void Restore()
+		{
+			i.m_maxSpeed = BaseMaxSpeed;
+			i.m_leanMultiplier = BaseLeanMultiplier;
+			i.m_acceleration = BaseAcceleration;
+			i.m_braking = BaseBraking;
+			i.m_turning = BaseTurning;
+		}
+	}
+
+	class SlowCitizenInfo
+	{
+		readonly CitizenInfo i;
+		public readonly float BaseWalkSpeed;
+
+		public SlowCitizenInfo(CitizenInfo i)
+		{
+			this.i = i;
+			BaseWalkSpeed = i.m_walkSpeed;
+		}
+
+		public void Apply(float walkSpeedMultiplier)
+		{
+			i.m_walkSpeed = BaseWalkSpeed * walkSpeedMultiplier;
+		}
+
+		public void Restore()
+		{
+			i.m_walkSpeed = BaseWalkSpeed;
 		}
 	}
 }
